26bedbe869cabec024ddd744086bcf94feb34d34
Tim Starr <timstarr14@gmail.com>
2021-03-23 16:50:48 +0000

    added start button and round board. Used js to keep spotlight inside game-container

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const spotlight = document.getElementById("spotlight");
+const gameContainer = document.getElementById("game-container");
 
 // connect mouse movements to #spotlight div
-document.addEventListener("mousemove", function(event) { 
-    let x = event.clientX;
-    let y = event.clientY;
+gameContainer.addEventListener("mousemove", (event) => { 
+    const x = event.clientX;
+    const y = event.clientY;
     spotlight.style.left = x + "px";
     spotlight.style.top = y + "px";
 });
 
+// hide square when outside spotlight
+
+
+
+
+
+
+
+
+
+
 // use getBoundingClientRect to determine the space within #game-container
 
-const gameContainer = document.getElementById("game-container");
+// const gameContainer = document.getElementById("game-container");
 
 // const gameContainerRect = gameContainer.getBoundingClientRect();
 // let x = gameContainerRect.left;
@@ -25,20 +37,24 @@ const gameContainer = document.getElementById("game-container");
 // OR: if #spotlight is inside gameContainerRect, then (run mousemove eventListener)
 
 // DETECTING WHETHER MOUSE IS INSIDE GAME CONTAINER OR NOT
-gameContainer.addEventListener("mouseenter", function(event) {
-    isInContainer = true;
-});
-gameContainer.addEventListener("mouseout", function(event) {
-    isInContainer = false;
-});
+// gameContainer.addEventListener("mouseenter", function(event) {
+//     isInContainer = true;
+// });
+// gameContainer.addEventListener("mouseout", function(event) {
+//     isInContainer = false;
+// });
 
-if (isInContainer === true) {
-    document.addEventListener("mousemove", function(event) { 
-        let x = event.clientX;
-        let y = event.clientY;
-        spotlight.style.left = x + "px";
-        spotlight.style.top = y + "px";
-    }) 
-} else {
-    spotlight != mousemove;
-};
+// if (isInContainer === true) {
+//     document.addEventListener("mousemove", function(event) { 
+//         let x = event.clientX;
+//         let y = event.clientY;
+//         spotlight.style.left = x + "px";
+//         spotlight.style.top = y + "px";
+//     }) 
+// } else {
+//     document.addEventListener("mousemove", function(event) { 
+//         let x = event.clientX;
+//         let y = event.clientY;
+//         spotlight.style.left != x + "px";
+//         spotlight.style.top != y + "px";
+// }
